6e23603651723f9a89d6dc835d83f2a468ee9faa
Renu6 <Renu.rachakulla@gmail.com>
2026-02-09 02:13:35 -0600

    Either passing class implementing Runnable or directly writing inline lambda for run method

--- src/concurrency/Main.java
+++ src/concurrency/Main.java
@@ -4,7 +4,10 @@ public class Main {
     static void main() {
         System.out.println("this is printed by the main thread : "+Thread.currentThread().getName());
         HelloWorldPrinter printer = new HelloWorldPrinter();
-        Thread t1 = new Thread(printer);
+//        Thread t1 = new Thread(printer);
+        Thread t1 = new Thread(() -> {
+            System.out.println("Hello world !"+Thread.currentThread().getName());
+        });
         t1.start();
 
     }
